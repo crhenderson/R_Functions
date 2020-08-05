be81cdd4ac13411afcf9f8aae79c516b310c3afd
crhenderson <coruhen@gmail.com>
2020-08-05 11:37:55 -0500

    Update sql.query.R
    changed readLines to scan

--- sql.query.R
+++ sql.query.R
@@ -1,7 +1,7 @@
 sql.query <- function(filename, odbc.connection.name, silent = TRUE) {
   #Read SQL File
   #Found here: http://stackoverflow.com/a/21344426
-  q <- readLines(filename, warn = !silent)
+  q <- scan(filename, what = 'character', fileEncoding = "UTF-16", sep="\n")
   q <- q[!grepl(pattern = "^\\s*--", x = q)] # remove full-line comments
   q <- sub(pattern = "--.*", replacement="", x = q) # remove midline comments
   q <- paste(q, collapse = " ")
